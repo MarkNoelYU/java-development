871f708322e060c2ebfc5515ae2fd715fac7e925
MarkNoelYU <markdnoel22@gmail.com>
2024-10-21 15:20:12 -0400

    Workbook 4 Initial Commit

---
+++ workbook-4/HotelOperations/src/main/java/HotelOperations.java
@@ -0,0 +1,44 @@
+public class HotelOperations {
+   private int roomNumber;
+    private String roomType;
+    private int numberOfBeds;
+    private boolean isOccupied;
+    private boolean isDirty;
+    private boolean isAvailable;
+
+    public int Room(int roomNumber, String roomType, double pricePerNight, int numberOfBeds) {
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.numberOfBeds = numberOfBeds;
+        this.isOccupied = false;
+        this.isDirty = false;
+        this.isAvailable = true;
+
+         int getNumberOfBeds; {
+            return numberOfBeds;
+        }
+        public void setNumberOfBeds(); {
+            this.numberOfBeds = numberOfBeds;
+        }
+          public double getPrice() {
+        }
+            public void setPrice()
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+
+}
+
+    private void setNumberOfBeds() {
+    }
+
+
